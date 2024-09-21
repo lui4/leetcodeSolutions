ee27f7c74d21ce25372a11b4da14f3cef407d49d
Luis <l.wiederhold@gmx.net>
2024-09-21 21:25:51 +0200

    NextGreaterElement.java Fixed Array Solution

--- src/NextGreaterElement.java
+++ src/NextGreaterElement.java
@@ -5,25 +5,20 @@ import java.util.Stack;
 public class NextGreaterElement {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> stack = new Stack<>();
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-
-        for (int i = 0; i < nums1.length; i++) {
-            hashMap.put(nums1[i], i);
-        }
+        int[] index = new int[10001]; // Array size is limited to 10k
 
         for (int i = nums2.length - 1;i >= 0; i--) {
 
             while (!stack.isEmpty() && nums2[i] > stack.peek()) {
                 stack.pop();
             }
-
-            if (hashMap.getOrDefault(nums2[i], -1) != -1) {
-                nums1[hashMap.get(nums2[i])] = stack.isEmpty() ? -1 : stack.peek();
-            }
+            index[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
 
             stack.push(nums2[i]);
         }
-
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = index[nums1[i]];
+        }
         return nums1;
     }
 
