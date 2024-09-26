0c4583726b1905b3e75d3028a7249f629e380069
lui4 <luis-buisiness@gmx.net>
2024-09-27 01:51:04 +0200

    Committed TreeNode.java and fixed related issues

--- src/ClosestBinarySearchTreeValue.java
+++ src/ClosestBinarySearchTreeValue.java
@@ -38,14 +38,4 @@ public class ClosestBinarySearchTreeValue {
 
     }
 
-    public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        node2.right = node3;
-        node2.left = node1;
-
-        ClosestBinarySearchTreeValue cb = new ClosestBinarySearchTreeValue();
-        System.out.println(cb.closestValue(node2, 0.14));
-    }
 }
---
+++ src/TreeNode.java
@@ -0,0 +1,8 @@
+public class TreeNode {
+    TreeNode right;
+    TreeNode left;
+    int val;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
