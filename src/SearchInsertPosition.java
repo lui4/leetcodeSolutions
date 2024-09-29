0a8e63e342f3bad3e6051dbd6c8051833317d309
lui4 <luis-buisiness@gmx.net>
2024-09-29 20:40:03 +0200

    SearchInsertPosition.java Solution

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,24 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            int num = nums[mid];
+
+            if (num == target) {
+                return mid;
+            }
+
+            if (num > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
