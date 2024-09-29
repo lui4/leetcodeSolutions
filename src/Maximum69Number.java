0d39ebdf7cdbe4d82721d1e46f5e5ed70129664a
lui4 <luis-buisiness@gmx.net>
2024-09-29 14:45:00 +0200

    Maximum69Number.java optimal Solution

--- src/Maximum69Number.java
+++ src/Maximum69Number.java
@@ -1,6 +1,18 @@
 public class Maximum69Number {
-    public int maximum69Number (int num) {
-        String number = String.valueOf(num);
-        return Integer.valueOf(number.replaceFirst("6", "9"));
+
+    public int maximum69Number(int num) {
+        int numCopy = num;
+        int indexSix = -1;
+        int currDigit = 0;
+
+        while (numCopy > 0) {
+            if (numCopy % 10 == 6)
+                indexSix = currDigit;
+
+            numCopy /= 10;
+            currDigit++;
+        }
+
+        return indexSix == -1 ? num : num + 3 * (int) Math.pow(10, indexSix);
     }
 }
