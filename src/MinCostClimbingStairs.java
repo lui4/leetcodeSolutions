4a5114e552213e291a9662aea6a28a203dfbb9e9
lui4 <luis-buisiness@gmx.net>
2024-10-02 13:10:46 +0200

    MinCostClimbingStairs.java Bottom-Up Solution

--- src/MinCostClimbingStairs.java
+++ src/MinCostClimbingStairs.java
@@ -3,25 +3,19 @@ import java.util.HashMap;
 public class MinCostClimbingStairs {
     HashMap<Integer, Integer> memory = new HashMap<>();
     public int minCostClimbingStairs(int[] cost) {
-        return dp(cost.length - 1, cost);
+        int bottomOne = 0;
+        int upperOne = 0;
+        int currentCost = 0;
+
+        for (int i = 2; i < cost.length + 1; i++) {
+            currentCost = Math.min(bottomOne + cost[i-2], upperOne + cost[i-1]);
+            bottomOne = upperOne;
+            upperOne = currentCost;
+        }
+
+        return upperOne;
     }
 
-    public int dp(int stair, int[] cost) {
-        if (stair == 0) {
-            return cost[0];
-        }
-
-        if(stair == 1) {
-            return Math.min(cost[0], cost[1]);
-        }
-
-        if (memory.getOrDefault(stair, null) != null) {
-            return memory.get(stair);
-        }
-
-        memory.put(stair, Math.min(dp(stair-1, cost) + cost[stair-1], dp(stair - 2, cost) + cost[stair - 2]));
-        return memory.get(stair);
-    }
 
     public static void main(String[] args) {
         MinCostClimbingStairs mc = new MinCostClimbingStairs();
