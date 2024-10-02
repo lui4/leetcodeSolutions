4598b1f474ac06dadde0bc22288b505147cee96b
lui4 <luis-buisiness@gmx.net>
2024-10-02 18:54:03 +0200

    CoinChange.java Bottom-Up Solution

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -1,10 +1,23 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class CoinChange {
     HashMap<Integer, Integer> memory = new HashMap<>();
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) return 0;
-        return dp(coins, amount);
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount+1);
+
+        dp[0] = 0;
+
+        for (int i = 0; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] < 0) continue;
+                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 
     public int dp(int[] coins, int amount) {
