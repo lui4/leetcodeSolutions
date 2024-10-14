a237d7a749efdb168434c39383b6c9ccb54a06f4
lui4 <luis-buisiness@gmx.net>
2024-10-15 00:20:40 +0200

    ReverseLinkedList.java Solution

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+
+    }
+}
